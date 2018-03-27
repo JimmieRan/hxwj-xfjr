504cb831cb51ad53f79c95d149bf0fa6cbcfbb8b
ranji <437578219@qq.com>
2018-03-27 10:40:50 +0800

    add

---
+++ src/main/java/com/hxwj/xfjr/modules/sys/redis/SysConfigRedis.java
@@ -0,0 +1,38 @@
+package com.hxwj.xfjr.modules.sys.redis;
+
+import com.hxwj.xfjr.common.utils.RedisKeys;
+import com.hxwj.xfjr.common.utils.RedisUtils;
+import com.hxwj.xfjr.modules.sys.entity.SysConfigEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 系统配置Redis
+ *
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017/7/18 21:08
+ */
+@Component
+public class SysConfigRedis {
+    @Autowired
+    private RedisUtils redisUtils;
+
+    public void saveOrUpdate(SysConfigEntity config) {
+        if(config == null){
+            return ;
+        }
+        String key = RedisKeys.getSysConfigKey(config.getKey());
+        redisUtils.set(key, config);
+    }
+
+    public void delete(String configKey) {
+        String key = RedisKeys.getSysConfigKey(configKey);
+        redisUtils.delete(key);
+    }
+
+    public SysConfigEntity get(String configKey){
+        String key = RedisKeys.getSysConfigKey(configKey);
+        return redisUtils.get(key, SysConfigEntity.class);
+    }
+}
